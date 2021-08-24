c2649bd0ce76250596536b6e9fc2c4c3cc632890
sebastiangonlai <sebastiangonlai@gmail.com>
2021-08-24 19:24:50 -0300

    postgres

--- src/main/java/logica/Plataforma.java
+++ src/main/java/logica/Plataforma.java
@@ -3,42 +3,47 @@ package logica;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name="Plataforma")
+@Entity
 public class Plataforma{
-	@Id
+    @Id
     private String nombre;
     private String descripcion;
     private String url;
-    
-	public Plataforma(String nombre, String descripcion, String url) {
 
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.url = url;
-	}
-	
-	public Plataforma() {
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public Plataforma(){
+	 super();
+    }
+
+    public Plataforma(String nombre, String descripcion, String url){
+	 super();
+	 this.nombre = nombre;
+	 this.descripcion = descripcion;
+	 this.url = url;
+    }
 
 
+    public String getNombre(){
+	 return nombre;
+    }
+
+    public void setNombre(String nombre){
+	 this.nombre = nombre;
+    }
+
+    public String getDescripcion(){
+	 return descripcion;
+    }
+
+    public void setDescripcion(String descripcion){
+	 this.descripcion = descripcion;
+    }
+
+    public String getUrl(){
+	 return url;
+    }
+
+    public void setUrl(String url){
+	 this.url = url;
+    }
 
 }
