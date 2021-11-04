95088647d72f151e30a7724b7565d3b79bcf13db
Aldrin R <aldrinrebella@gmail.com>
2021-11-03 23:13:07 -0300

    plataforma

--- src/main/java/test/PlataformasTest.java
+++ src/main/java/test/PlataformasTest.java
@@ -1,13 +1,17 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.List;
 
 import javax.xml.transform.OutputKeys;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import datatypes.DtPlataforma;
 import excepciones.PlataformaRepetidaExcepcion;
@@ -20,25 +24,48 @@ public class PlataformasTest {
 	IControladorPlataforma iconPlat = Fabrica.getInstancia().getIControladorPlataforma();
 	DtPlataforma dt;
 	
-	@Before
-	public void beforeTest() {
-		dt = new DtPlataforma("Plataforma1Test", "Descripcion", "Url");
-	}
 	
 	@Test
-	public void altaPlataformaTest() {
-		try {
-			dt = new DtPlataforma("Plataforma1Test", "Descripcion", "Url");
+	public void altaPlataformaTest() throws PlataformaRepetidaExcepcion {
+		
+			dt = new DtPlataforma("Plataforma2Test", "Descripcion", "Url");
 			iconPlat.altaPlataforma(dt);
 			Plataforma dtBd = iconPlat.buscarPlataforma(dt.getNombre());
 			assertEquals(dt.getNombre(),dtBd.getNombre());
-		} catch (PlataformaRepetidaExcepcion e) {
-			fail("Plataforma repetida");
-		}
-	}
-	
-	@After
-	public void afterTest() {
 		
 	}
+	
+	@Test
+	public void listarPlataformaTest() throws PlataformaRepetidaExcepcion {
+	
+			dt = new DtPlataforma("Plataforma3Test", "Descripcion", "Url");
+			iconPlat.altaPlataforma(dt);
+			dt = new DtPlataforma("Plataforma4Test", "Descripcion", "Url");
+			iconPlat.altaPlataforma(dt);
+			dt = new DtPlataforma("Plataforma5Test", "Descripcion", "Url");
+			iconPlat.altaPlataforma(dt);
+			List<DtPlataforma> lista = iconPlat.listarPlataformas();
+			List<String> listastr = iconPlat.listarPlataformasStr();
+			assertTrue(
+			((lista.stream().anyMatch(a -> (a.getNombre() == "Plataforma3Test")))&& 
+			(lista.stream().anyMatch(a -> (a.getNombre() == "Plataforma4Test")))&& 
+			(lista.stream().anyMatch(a -> (a.getNombre() == "Plataforma5Test")))) && 
+			((listastr.stream().anyMatch(a -> (a == "Plataforma3Test")))&& 
+			(listastr.stream().anyMatch(a -> (a == "Plataforma4Test")))&& 
+			(listastr.stream().anyMatch(a -> (a == "Plataforma5Test"))))	
+			);
+	
+			
+		
+	}
+	@Test(expected = PlataformaRepetidaExcepcion.class)
+	public void plataformaRepetidaTest() throws PlataformaRepetidaExcepcion {
+		
+			dt = new DtPlataforma("Plataforma6Test", "Descripcion", "Url");
+			iconPlat.altaPlataforma(dt);
+			dt = new DtPlataforma("Plataforma6Test", "Descripcion", "Url");
+			iconPlat.altaPlataforma(dt);
+			
+	}
+	
 }
