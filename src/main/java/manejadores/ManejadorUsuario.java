ff0f821a434ecb9a9cad62e8de01114b66c29f25
braulio12 <brauliojaureguy12@gmail.com>
2022-04-14 13:43:29 -0300

    quitado de metodo en desuso

--- src/main/java/manejadores/ManejadorUsuario.java
+++ src/main/java/manejadores/ManejadorUsuario.java
@@ -67,15 +67,6 @@ public class ManejadorUsuario{
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Usuario> obtenerUsuario(){
-		Conexion conexion = Conexion.getInstancia();
-		EntityManager em = conexion.getEntityManager();
-		Query query = em.createQuery("select u from Usuario u");
-		List<Usuario> listUsuario = (List<Usuario>) query.getResultList();
-		return listUsuario;
-	}
-
-	@SuppressWarnings("unchecked")
 	public List<Usuario> listarUsuarios(){
 		Conexion conexion = Conexion.getInstancia();
 		EntityManager em = conexion.getEntityManager();
